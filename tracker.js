825de92e3636f802263198ad4ff4921dc359c564
Evan Ahmed <167888159+evanahmedk@users.noreply.github.com>
2025-02-02 06:30:06 +0500

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,29 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const userIP = async () => {
-    const response = await fetch('https://api64.ipify.org?format=json');
-    const data = await response.json();
-    return data.ip;
-  };
-
-  const sendToTelegram = async (message) => {
-    const botToken = '7409054007:AAEpqTyev5AKyz-n7s2sPps7lLWH4Snl7a4';
-    const chatId = '@Usertrackerofficial_bot';
-    const telegramURL = `https://api.telegram.org/bot${botToken}/sendMessage`;
-
-    await fetch(telegramURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: message
-      })
-    });
-  };
-
-  userIP().then(ip => {
-    const userInfo = `New Visitor:\nIP: ${ip}\nTime: ${new Date().toLocaleString()}`;
-    sendToTelegram(userInfo);
-  });
-});
+{
+    "bot_token": "7409054007:AAEpqTyev5AKyz-n7s2sPps7lLWH4Snl7a4",
+    "chat_id": "@Usertrackerofficial_bot",
+    "webhook_url": "https://user-tracker-kvps-4175iqjht-sandra-hymans-projects.vercel.app/"
+}
